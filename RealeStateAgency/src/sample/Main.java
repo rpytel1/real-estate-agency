1298d48b10f96f9b064c5ac0f05475ee0b370380
rpytel1 <rafal.pytel1@wp.pl>
2017-01-12 14:29:12 +0100

    basic view

--- RealeStateAgency/src/sample/Main.java
+++ RealeStateAgency/src/sample/Main.java
@@ -1,21 +1,158 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellEditEvent;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private TableView<Person> table = new TableView<Person>();
+    BorderPane border = new BorderPane();
+    private final ObservableList<Person> data =
+            FXCollections.observableArrayList(
+                    new Person("Jacob", "Smith", "jacob.smith@example.com"),
+                    new Person("Isabella", "Johnson", "isabella.johnson@example.com"),
+                    new Person("Ethan", "Williams", "ethan.williams@example.com"),
+                    new Person("Emma", "Jones", "emma.jones@example.com"),
+                    new Person("Michael", "Brown", "michael.brown@example.com"));
+    final HBox hb = new HBox();
+
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+        Scene scene = new Scene(new Group());
+        stage.setTitle("Real Estate App");
+        stage.setWidth(800);
+        stage.setHeight(600);
+
+        tableHandle();
+
+        final VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(10, 0, 0, 10));
+        vbox.getChildren().addAll(table, hb);
+        final VBox vboxOptions = new VBox();
+        vboxOptions.setSpacing(5);
+        vboxOptions.setPadding(new Insets(10, 0, 0, 10));
+        ;
+        Label label1 = new Label("Add new Query");
+        Button button = new Button("Find best estates for clients");
+        Button addTotallyNewEstate = new Button("Add totally new Estate");
+        Button findNewDevOffers = new Button("Find new offers from Developer");
+        vboxOptions.getChildren().addAll(label1, button, addTotallyNewEstate, findNewDevOffers);
+        final HBox hboxMain = new HBox();
+        hboxMain.setSpacing(5);
+        hboxMain.setPadding(new Insets(10, 0, 0, 10));
+        hboxMain.getChildren().addAll(vbox, vboxOptions);
+
+        ((Group) scene.getRoot()).getChildren().addAll(hboxMain);
+
+        ;
+
+        stage.setScene(scene);
+        stage.show();
     }
 
+    public void tableHandle() {
+        table.setEditable(true);
+
+        TableColumn firstNameCol = new TableColumn("First Name");
+        firstNameCol.setMinWidth(100);
+        firstNameCol.setCellValueFactory(
+                new PropertyValueFactory<Person, String>("firstName"));
+        firstNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        firstNameCol.setOnEditCommit(
+                new EventHandler<CellEditEvent<Person, String>>() {
+                    @Override
+                    public void handle(CellEditEvent<Person, String> t) {
+                        ((Person) t.getTableView().getItems().get(
+                                t.getTablePosition().getRow())
+                        ).setFirstName(t.getNewValue());
+                    }
+                }
+        );
+
+
+        TableColumn lastNameCol = new TableColumn("Last Name");
+        lastNameCol.setMinWidth(100);
+        lastNameCol.setCellValueFactory(
+                new PropertyValueFactory<Person, String>("lastName"));
+        lastNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        lastNameCol.setOnEditCommit(
+                new EventHandler<CellEditEvent<Person, String>>() {
+                    @Override
+                    public void handle(CellEditEvent<Person, String> t) {
+                        ((Person) t.getTableView().getItems().get(
+                                t.getTablePosition().getRow())
+                        ).setLastName(t.getNewValue());
+                    }
+                }
+        );
+
+        TableColumn emailCol = new TableColumn("Email");
+        emailCol.setMinWidth(200);
+        emailCol.setCellValueFactory(
+                new PropertyValueFactory<Person, String>("email"));
+        emailCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        emailCol.setOnEditCommit(
+                new EventHandler<CellEditEvent<Person, String>>() {
+                    @Override
+                    public void handle(CellEditEvent<Person, String> t) {
+                        ((Person) t.getTableView().getItems().get(
+                                t.getTablePosition().getRow())
+                        ).setEmail(t.getNewValue());
+                    }
+                }
+        );
+
+        table.setItems(data);
+        table.getColumns().addAll(firstNameCol, lastNameCol, emailCol);
+
+        final TextField addFirstName = new TextField();
+        addFirstName.setPromptText("First Name");
+        addFirstName.setMaxWidth(firstNameCol.getPrefWidth());
+        final TextField addLastName = new TextField();
+        addLastName.setMaxWidth(lastNameCol.getPrefWidth());
+        addLastName.setPromptText("Last Name");
+        final TextField addEmail = new TextField();
+        addEmail.setMaxWidth(emailCol.getPrefWidth());
+        addEmail.setPromptText("Email");
+
+        final Button addButton = new Button("Add");
+        addButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                data.add(new Person(
+                        addFirstName.getText(),
+                        addLastName.getText(),
+                        addEmail.getText()));
+                addFirstName.clear();
+                addLastName.clear();
+                addEmail.clear();
+            }
+        });
+
+        hb.getChildren().addAll(addFirstName, addLastName, addEmail, addButton);
+        hb.setSpacing(3);
+    }
 
     public static void main(String[] args) {
         launch(args);
---
+++ RealeStateAgency/src/sample/Person.java
@@ -0,0 +1,45 @@
+package sample;
+
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ * Created by Rafal on 2017-01-12.
+ */
+public class Person {
+
+
+    private final SimpleStringProperty firstName;
+    private final SimpleStringProperty lastName;
+    private final SimpleStringProperty email;
+
+    public Person(String fName, String lName, String email) {
+        this.firstName = new SimpleStringProperty(fName);
+        this.lastName = new SimpleStringProperty(lName);
+        this.email = new SimpleStringProperty(email);
+    }
+
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    public void setFirstName(String fName) {
+        firstName.set(fName);
+    }
+
+    public String getLastName() {
+        return lastName.get();
+    }
+
+    public void setLastName(String fName) {
+        lastName.set(fName);
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public void setEmail(String fName) {
+        email.set(fName);
+    }
+}
+
